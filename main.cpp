a80eefd659ce4e9b65411c6fc3f6924c86aff3ba
Hasana Manandhar <manandharhasana07@gmail.com>
2024-02-06 23:38:14 +0545

    Implemented random number generator to display random phrase from file

--- main.cpp
+++ main.cpp
@@ -2,37 +2,53 @@
 #include <json/json.h>
 #include <typeinfo>
 #include <fstream>
+#include <cstdlib>
+#include <ctime>
 using namespace std;
 using namespace Json;
 
 void readFromFile(string Cat)
 {
     Value word;
+
+    //Opens the json file where words for games are stored
+
     ifstream word_file("wordsAndHints.json", std::ifstream::binary);
     word_file >> word;
-    cout << typeid(word).name();
+
+    //Randomly generate words of Category selected by users
+
+    srand(time(0));
+    int randomIndex = rand() % 5;
+    Value wordToGuess = word["categories"][Cat][randomIndex]["phrase"];
+    Value hint = word["categories"][Cat][randomIndex]["hint"];
+    cout << "Word to guess ===>" << wordToGuess << endl;
+    cout << "Hint===>" << hint;
+
+    word_file.close();
 }
 
-
+void play(string);
 
 void CategorySelection()
 {
     int Category;
-    cout<<"Which Category do you want to play?"<<endl;
-    cout<<"1: Places  2: Movies/Series"<<endl;
-    cout<<"Enter here: .....";
-    cin>>Category;
+    cout << "Which Category do you want to play?" << endl;
+    cout << "1: Places  2: Movies/Series" << endl;
+    cout << "Enter here: .....";
+    cin >> Category;
     switch (Category)
     {
     case 1:
         readFromFile("places");
         break;
-    
+    case 2:
+        readFromFile("series");
+        break;
     default:
         break;
     }
 }
-
 
 int main()
 {
